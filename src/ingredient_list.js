6457bf9fc97c3f053ce7f2743d2079d909f89052
Pau Perez <saulopefa@gmail.com>
2018-09-03 20:12:42 +0200

    Allow editing item when clicking on it

--- src/ingredient_list.js
+++ src/ingredient_list.js
@@ -15,15 +15,95 @@ function IngredientList(props) {
   );
 }
 
-function IngredientListItem(props) {
-  return (
-    <li
-      onClick={() => props.onClick(props.id)}
-      className={props.collected ? 'disabled' : 'enabled'}
-    >
-      {props.name}
-    </li>
-  );
+class ListItemForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: props.value };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  render() {
+    return (
+      <input
+        type="text"
+        value={this.state.value}
+        onChange={this.handleChange}
+        onBlur={this.props.onBlur}
+        onKeyDown={this.props.onKeyDown}
+      />
+    );
+  }
+}
+
+class IngredientListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: props.name,
+      editing: false
+    }
+
+    this.toggleEditing = this.toggleEditing.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
+
+  toggleEditing() {
+    this.setState((prevState, props) => {
+      return { editing: !prevState.editing };
+    });
+  }
+
+  handleBlur(event) {
+    this.setState({
+      editing: false,
+      name: this.props.name
+    });
+  }
+
+  handleKeyDown(event) {
+    const ENTER_KEY = 13;
+
+    if (event.which === ENTER_KEY) {
+      this.setState({
+        editing: false,
+        name: event.target.value
+      });
+    }
+  }
+
+  render() {
+    let item;
+
+    if (this.state.editing) {
+      item = <ListItemForm
+        value={this.state.name}
+        onBlur={this.handleBlur}
+        onKeyDown={this.handleKeyDown}
+      />
+    }
+    else {
+      item = <a onClick={this.toggleEditing}>{this.state.name}</a>
+    }
+
+    return (
+      <li
+        className={this.props.collected ? 'disabled' : 'enabled'}
+      >
+        <input
+          type="checkbox"
+          checked={this.props.collected}
+          onChange={() => this.props.onClick(this.props.id)}
+        />
+        {item}
+      </li>
+    );
+  }
 }
 
 export default IngredientList;
