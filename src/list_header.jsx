52bcf6071340401cd26a9be80ed25ef9d8def7ee
Pau Perez <saulopefa@gmail.com>
2018-09-08 15:49:25 +0200

    Import ListHeader SCSS styles in component

--- src/list_header.jsx
+++ src/list_header.jsx
@@ -1,23 +1,19 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import './ListHeader.css';
 
 function ListHeader(props) {
   const { listName, count } = props;
 
-  const iconStyles = {
-    color: 'lightseagreen',
-    margin: '0.7rem 0.5rem 0'
-  };
-
   return (
-    <header className='ingredient-list-header'>
+    <header className="ListHeader">
       <p className='subtle'>{count} ingredients</p>
       <div className='title'>
 	<h1>{listName}</h1>
 	<FontAwesomeIcon
 	  icon='pencil-alt'
-	  className='action-icon'
-	  style={iconStyles}
+	  className="ActionIcon"
 	/>
     </div>
   </header>
