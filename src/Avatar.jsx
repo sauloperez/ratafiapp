d3f3d1b77b735f019945b31094972ab5917fac4f
Pau Perez <saulopefa@gmail.com>
2018-10-16 20:07:13 +0200

    Replace class with className

--- src/Avatar.jsx
+++ src/Avatar.jsx
@@ -7,8 +7,8 @@ import Icon from './Icon';
 class Avatar extends React.Component {
   render() {
     return (
-      <div class="Avatar">
-        <img class="Avatar__picture" src="https://pbs.twimg.com/profile_images/820296060316721153/DuXixESD_bigger.jpg" alt="avatar"  width="28" height="28"/>
+      <div className="Avatar">
+        <img className="Avatar__picture" src="https://pbs.twimg.com/profile_images/820296060316721153/DuXixESD_bigger.jpg" alt="avatar"  width="28" height="28"/>
         <Icon icon="chevron-down" />
       </div>
     );
