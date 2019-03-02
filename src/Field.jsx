3fbafad6bfb7c4e9ee579f65f1458aafe5932e57
Pau Perez <saulopefa@gmail.com>
2019-03-02 11:40:04 +0100

    Fix form Field width

--- src/Field.jsx
+++ src/Field.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 import './Field.css';
 
 function Field(props) {
-  const { label, style } = props;
+  const { label, style, ...otherProps } = props;
 
   return (
     <div className="Field" style={ style }>
       <label>{ label }</label>
-      <input type="text" {...props} />
+      <input type="text" {...otherProps} />
     </div>
   );
 }
