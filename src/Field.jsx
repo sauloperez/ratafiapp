48f5c6e3c94db6660be484e4c78b95e742df58cc
Pau Perez <saulopefa@gmail.com>
2019-03-09 19:25:30 +0100

    Add isInvalid prop type to render errored field

--- src/Field.jsx
+++ src/Field.jsx
@@ -2,15 +2,28 @@ import React from 'react';
 
 import './Field.css';
 
-function Field(props) {
-  const { label, style, ...otherProps } = props;
+class Field extends React.Component {
+  getClassName() {
+    const { isInvalid } = this.props;
+    let className = 'Field';
 
-  return (
-    <div className="Field Field--error" style={style}>
-      <label>{ label }</label>
-      <input type="text" {...otherProps} />
-    </div>
-  );
+    if (isInvalid) {
+      className += ' Field--error';
+    }
+
+    return className;
+  }
+
+  render() {
+    const { label, style, isInvalid, ...otherProps } = this.props;
+
+    return (
+      <div className={this.getClassName()} style={style}>
+        <label>{ label }</label>
+        <input type="text" {...otherProps} />
+      </div>
+    );
+  }
 }
 
 export default Field;
