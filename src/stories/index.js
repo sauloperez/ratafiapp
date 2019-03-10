640014204db266d03a3d25f298970debb247fe92
Pau Perez <saulopefa@gmail.com>
2019-03-10 12:53:10 +0100

    Add focus styles for Field

--- src/stories/index.js
+++ src/stories/index.js
@@ -6,8 +6,13 @@ import '../index.css';
 import Field from '../Field';
 
 storiesOf('Field', module)
-  .add('regular', () => <Field style={{ width: '120px' }} />)
-  .add('active', () => <Field style={{ width: '120px' }} />)
+  .add('regular', () => <Field style={{ width: '120px' }} label="Nom" />)
+  .add('focus', () => (
+    <Field
+      style={{ width: '120px' }}
+      autoFocus
+      label="Nom"
+    />))
   .add('invalid', () => (
     <Field
       isInvalid
